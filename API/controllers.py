cd5131055e478dc4f6365a8165073bba2053a586
Klim K <kirklimushin@gmail.com>
2022-06-05 14:31:28 +0300

    Edited

---
+++ API/controllers.py
@@ -0,0 +1,18 @@
+import http.client
+import fastapi, fastapi_csrf_protect
+from .settings import application
+
+
+@application.get('/healthcheck/')
+def healthcheck(csrf_protect: fastapi_csrf_protect.CsrfProtect = fastapi.Depends()):
+    response = fastapi.responses.Response(status_code=200)
+    return response
+
+@application.get('/get/csrf/token/')
+def get_csrf_token(csrf_protect: fastapi_csrf_protect.CsrfProtect = fastapi.Depends()):
+    response = fastapi.responses.Response(status_code=200)
+    csrf_protect.set_csrf_cookie(response=response)
+    return response
+
+
+
